e581e855a3e23c0ca5f32aec7b03ec7b5887e35d
Kirsten Greed <kirsteng@jobtalk.com.au>
2023-03-06 21:09:56 +1100

    Add project files.

---
+++ Catz.Module/BusinessObjects/CatzDbContext.cs
@@ -0,0 +1,94 @@
+﻿using DevExpress.ExpressApp.EFCore.Updating;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
+using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.ExpressApp.Design;
+using DevExpress.ExpressApp.EFCore.DesignTime;
+using DevExpress.Persistent.BaseImpl.EFCore.AuditTrail;
+
+namespace Catz.Module.BusinessObjects;
+
+// This code allows our Model Editor to get relevant EF Core metadata at design time.
+// For details, please refer to https://supportcenter.devexpress.com/ticket/details/t933891.
+public class CatzContextInitializer : DbContextTypesInfoInitializerBase {
+	protected override DbContext CreateDbContext() {
+		var optionsBuilder = new DbContextOptionsBuilder<CatzEFCoreDbContext>()
+            .UseSqlServer(";")
+            .UseChangeTrackingProxies()
+            .UseObjectSpaceLinkProxies();
+        return new CatzEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+//This factory creates DbContext for design-time services. For example, it is required for database migration.
+public class CatzDesignTimeDbContextFactory : IDesignTimeDbContextFactory<CatzEFCoreDbContext> {
+	public CatzEFCoreDbContext CreateDbContext(string[] args) {
+		throw new InvalidOperationException("Make sure that the database connection string and connection provider are correct. After that, uncomment the code below and remove this exception.");
+		//var optionsBuilder = new DbContextOptionsBuilder<CatzEFCoreDbContext>();
+		//optionsBuilder.UseSqlServer("Integrated Security=SSPI;Pooling=false;Data Source=(localdb)\\mssqllocaldb;Initial Catalog=Catz");
+        //optionsBuilder.UseChangeTrackingProxies();
+        //optionsBuilder.UseObjectSpaceLinkProxies();
+		//return new CatzEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+[TypesInfoInitializer(typeof(CatzContextInitializer))]
+public class CatzEFCoreDbContext : DbContext {
+	public CatzEFCoreDbContext(DbContextOptions<CatzEFCoreDbContext> options) : base(options) {
+	}
+	//public DbSet<ModuleInfo> ModulesInfo { get; set; }
+	public DbSet<ModelDifference> ModelDifferences { get; set; }
+	public DbSet<ModelDifferenceAspect> ModelDifferenceAspects { get; set; }
+	public DbSet<PermissionPolicyRole> Roles { get; set; }
+	public DbSet<Catz.Module.BusinessObjects.ApplicationUser> Users { get; set; }
+    public DbSet<Catz.Module.BusinessObjects.ApplicationUserLoginInfo> UserLoginInfos { get; set; }
+    public DbSet<AuditDataItemPersistent> AuditData { get; set; }
+    public DbSet<AuditEFCoreWeakReference> AuditEFCoreWeakReference { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.HasChangeTrackingStrategy(ChangeTrackingStrategy.ChangingAndChangedNotificationsWithOriginalValues);
+        modelBuilder.Entity<Catz.Module.BusinessObjects.ApplicationUserLoginInfo>(b => {
+            b.HasIndex(nameof(DevExpress.ExpressApp.Security.ISecurityUserLoginInfo.LoginProviderName), nameof(DevExpress.ExpressApp.Security.ISecurityUserLoginInfo.ProviderUserKey)).IsUnique();
+        });
+        modelBuilder.Entity<AuditEFCoreWeakReference>()
+            .HasMany(p => p.AuditItems)
+            .WithOne(p => p.AuditedObject);
+        modelBuilder.Entity<AuditEFCoreWeakReference>()
+            .HasMany(p => p.OldItems)
+            .WithOne(p => p.OldObject);
+        modelBuilder.Entity<AuditEFCoreWeakReference>()
+            .HasMany(p => p.NewItems)
+            .WithOne(p => p.NewObject);
+        modelBuilder.Entity<AuditEFCoreWeakReference>()
+            .HasMany(p => p.UserItems)
+            .WithOne(p => p.UserObject);
+        modelBuilder.Entity<ModelDifference>()
+            .HasMany(t => t.Aspects)
+            .WithOne(t => t.Owner)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
+
+public class CatzAuditingDbContext : DbContext {
+    public CatzAuditingDbContext(DbContextOptions<CatzAuditingDbContext> options) : base(options) {
+    }
+    public DbSet<AuditDataItemPersistent> AuditData { get; set; }
+    public DbSet<AuditEFCoreWeakReference> AuditEFCoreWeakReference { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.HasChangeTrackingStrategy(ChangeTrackingStrategy.ChangingAndChangedNotificationsWithOriginalValues);
+        modelBuilder.Entity<AuditEFCoreWeakReference>()
+            .HasMany(p => p.AuditItems)
+            .WithOne(p => p.AuditedObject);
+        modelBuilder.Entity<AuditEFCoreWeakReference>()
+            .HasMany(p => p.OldItems)
+            .WithOne(p => p.OldObject);
+        modelBuilder.Entity<AuditEFCoreWeakReference>()
+            .HasMany(p => p.NewItems)
+            .WithOne(p => p.NewObject);
+        modelBuilder.Entity<AuditEFCoreWeakReference>()
+            .HasMany(p => p.UserItems)
+            .WithOne(p => p.UserObject);
+    }
+}
---
+++ Catz.Win/WinApplication.cs
@@ -0,0 +1,82 @@
+﻿using Catz.Module.BusinessObjects;
+using DevExpress.EntityFrameworkCore.Security;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.EFCore;
+using DevExpress.ExpressApp.Security;
+using DevExpress.ExpressApp.Win;
+using DevExpress.ExpressApp.Win.Utils;
+using DevExpress.Persistent.BaseImpl.EFCore.AuditTrail;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catz.Win;
+// For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.Win.WinApplication._members
+
+public class CatzWindowsFormsApplication : WinApplication
+{
+    public CatzWindowsFormsApplication()
+    {
+        SplashScreen = new DXSplashScreen(typeof(XafSplashScreen), new DefaultOverlayFormOptions());
+        ApplicationName = "Catz";
+        CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+        UseOldTemplates = false;
+        DatabaseVersionMismatch += CatzWindowsFormsApplication_DatabaseVersionMismatch;
+        CustomizeLanguagesList += CatzWindowsFormsApplication_CustomizeLanguagesList;
+    }
+
+    protected override void CreateDefaultObjectSpaceProvider(CreateCustomObjectSpaceProviderEventArgs args)
+    {
+        var connectionString = args.ConnectionString;
+        var handler = new EFCoreDatabaseProviderHandler<CatzEFCoreDbContext>(MakeBuilder, connectionString); // fails here with error cs0149 Method name expected.
+        var efCoreObjectSpaceProvider = new SecuredEFCoreObjectSpaceProvider<CatzEFCoreDbContext>(
+            (ISelectDataSecurityProvider)Security,
+            handler);
+        args.ObjectSpaceProviders.Add(efCoreObjectSpaceProvider);
+        args.ObjectSpaceProviders.Add(new NonPersistentObjectSpaceProvider(TypesInfo, null));
+    }
+
+    private DbContextOptionsBuilder<CatzEFCoreDbContext> MakeBuilder(string connectionString)
+    {
+        var builder = new DbContextOptionsBuilder<CatzEFCoreDbContext>();
+        builder.UseSqlServer(connectionString);
+        builder.UseSecurity((ISelectDataSecurityProvider)Security);
+        builder.UseAudit();
+        return builder;
+    }
+
+    private void CatzWindowsFormsApplication_CustomizeLanguagesList(object sender, CustomizeLanguagesListEventArgs e)
+    {
+        string userLanguageName = System.Threading.Thread.CurrentThread.CurrentUICulture.Name;
+        if(userLanguageName != "en-US" && e.Languages.IndexOf(userLanguageName) == -1)
+        {
+            e.Languages.Add(userLanguageName);
+        }
+    }
+
+    private void CatzWindowsFormsApplication_DatabaseVersionMismatch(
+        object sender,
+        DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs e)
+    {
+#if EASYTEST
+        e.Updater.Update();
+        e.Handled = true;
+#else
+        if(System.Diagnostics.Debugger.IsAttached)
+        {
+            e.Updater.Update();
+            e.Handled = true;
+        } else
+        {
+            string message = "The application cannot connect to the specified database, " +
+                "because the database doesn't exist, its version is older " +
+                "than that of the application or its schema does not match " +
+                "the ORM data model structure. To avoid this error, use one " +
+                "of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
+            if(e.CompatibilityError != null && e.CompatibilityError.Exception != null)
+            {
+                message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
+            }
+            throw new InvalidOperationException(message);
+        }
+#endif
+    }
+}
